9474ce655a15e91076480bd4ead42722b49414dd
R Vigneswar <vigneshr.1894@gmail.com>
2023-10-24 12:17:58 +0530

    added generate ssh keys script

--- Custom_bash_scripts/shell_env_setup.sh
+++ Custom_bash_scripts/shell_env_setup.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
+oh_my_zsh="$HOME/.oh_my_zsh"
+EMAIL="vigneshr.1894@gmail.com"
+
 install_dependencies() {
     if [ -x "$(command -v zsh)" ]; then
         echo "ZSH is already installed"
     else
         echo "ZSH is not installed, installing zsh."
-        apt update
-        apt install zsh zsh-common curl git  -y
+        sudo apt update
+        sudo apt install zsh zsh-common curl git  -y
     fi
     if [ -x "$(command -v curl)" ]; then
         echo "Curl is already installed."
     else
         echo "Installing Curl."
-        apt install curl -y
+        sudo apt install curl -y
     fi
     if [ -x "$(command -v git)" ]; then
         echo "Git is already isntalled."
     else
         echo "Installing Git."
-        apt isntall git -y
+        sudo apt isntall git -y
     fi
 }
-
-
-oh_my_zsh="$HOME/.oh_my_zsh"
 
 install_ohmyzsh() {
     if [ -d "$oh_my_zsh" ]; then
         echo "Oh My Zsh is isntalled."
     else
         echo "Oh My Zsh is not installed, installing."
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     fi
 }
 
@@ -45,6 +45,15 @@ install_omz_plugings() {
     git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
 }
 
+generate_ssh_key() {
+    echo "Generating ssh keys"
+    ssh-keygen -t rsa -C "$EMAIL"
+    echo "Key Generation Successfull"
+    echo "showing public key"
+    cat ~/.ssh/*.pub
+}
+
 install_dependencies
 install_ohmyzsh
 install_omz_plugings
+generate_ssh_key
